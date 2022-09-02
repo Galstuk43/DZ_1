b6609e8e0b42b2e656dc2a25287cbe1fd212a387
Galstuk <galstuk43@mail.ru>
2022-09-02 15:05:58 +0300

    Задача 2

---
+++ Task_02/Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.WriteLine("максимальное значение для двух чисел");
+            Console.Write("Введите первое число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int max;
+            if (a > b)
+            {
+               max = a;
+               
+            }
+            else
+            {
+               max = b;
+            }
+            Console.WriteLine(max);
