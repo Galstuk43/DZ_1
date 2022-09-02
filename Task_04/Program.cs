491c5bbe6584755374a3f31813fddd0b3416e500
Galstuk <galstuk43@mail.ru>
2022-09-02 15:27:08 +0300

    Задача 4

---
+++ Task_04/Program.cs
@@ -0,0 +1,14 @@
+﻿ Console.WriteLine("максимальное значение для трех чисел");
+            Console.Write("Введите первое число ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число ");
+            int c =  Convert.ToInt32(Console.ReadLine());
+            int max = a;
+            if (max < b)
+               max = b; 
+            if (max < c)
+               max = c;
+            
+            Console.WriteLine(max);
